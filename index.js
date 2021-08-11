a728bc4090d857274542ec500a7643ee994edd2f
Gospel Chukwu <brondchux@gmail.com>
2021-08-11 00:06:16 -0400

    Replaced all import-statment with const-className to avoid the import error. Also I exported all class modules now

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // PSEUDOCODE ======================
+
 // DEPENDENCES ======================
+const Intern = require("./lib/intern");
+
 // DATA ======================
+
 // FUNCTIONS ======================
+
 // INITIALIZATION ======================
+const intern = new Intern(9061, "Sam", "sam@gmail.com", "Columbia University"); // just testing
+console.log(intern.getSchool());
